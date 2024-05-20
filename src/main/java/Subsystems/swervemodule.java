17406910d5da558565707eef231e63f8cc383ed0
Imcab <142848134+Imcab@users.noreply.github.com>
2024-05-20 12:02:45 -0600

    Subsystem module creation :3

---
+++ src/main/java/Subsystems/swervemodule.java
@@ -0,0 +1,80 @@
+package Subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class swervemodule extends SubsystemBase{
+    
+    CANSparkMax m_drive;
+    CANSparkMax m_turn;
+
+    AnalogInput absoluteEncoder;
+    PIDController PID;
+
+    int number;
+
+    public swervemodule(int moduleNumber, int ID_drive, int ID_turn, int portencoder, int kP, int kI, int kD, boolean DReversed, boolean TReversed){
+     
+        m_drive = new CANSparkMax(ID_drive, MotorType.kBrushless);
+        m_turn = new CANSparkMax(ID_turn, MotorType.kBrushless);
+
+        absoluteEncoder = new AnalogInput(portencoder);
+
+        PID = new PIDController(kP, kI, kD);
+
+        
+        m_drive.setInverted(DReversed);
+        m_turn.setInverted(TReversed);
+
+        number = moduleNumber;
+
+    }
+
+    public Rotation2d AngleEncoder(){
+
+        double encoderBits = absoluteEncoder.getValue();
+        double angleEncoder = (encoderBits * 360) / 4096;
+
+        return Rotation2d.fromDegrees(angleEncoder);
+
+    }
+
+    public void setSpeed(SwerveModuleState desiredState){
+
+        m_drive.set(desiredState.speedMetersPerSecond);
+
+    }
+
+    public void setAngle(SwerveModuleState desiredState){
+
+        double PIDvalue = PID.calculate(AngleEncoder().getDegrees(), desiredState.angle.getDegrees());
+
+        m_turn.set(PIDvalue);
+
+    }
+
+    public void desiredState(SwerveModuleState desiredState){
+
+        desiredState = SwerveModuleState.optimize(desiredState, AngleEncoder());
+
+        setSpeed(desiredState);
+        setAngle(desiredState);
+
+    }
+
+    @Override
+    public void periodic (){
+
+        SmartDashboard.putNumber("Angle Encoder "+ number, AngleEncoder().getDegrees());
+    }
+ 
+}
+
+// :v
---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,65 @@
+package frc.robot;
+
+public class Constants {
+
+    public static final class frontLeft{
+
+        public static final int DrivePort = 0;
+        public static final int TurnPort = 0;
+        public static final int EncPort = 0;
+ 
+        public static final boolean DrivemotorReversed = false;
+        public static final boolean TurnmotorReversed = false;
+ 
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+    }
+
+    public static final class frontRight{
+
+        public static final int DrivePort = 0;
+        public static final int TurnPort = 0;
+        public static final int EncPort = 0;
+ 
+        public static final boolean DrivemotorReversed = false;
+        public static final boolean TurnmotorReversed = false;
+ 
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+    }
+
+    public static final class backLeft{
+
+        public static final int DrivePort = 0;
+        public static final int TurnPort = 0;
+        public static final int EncPort = 0;
+ 
+        public static final boolean DrivemotorReversed = false;
+        public static final boolean TurnmotorReversed = false;
+ 
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+    }
+
+    public static final class backRight{
+
+        public static final int DrivePort = 0;
+        public static final int TurnPort = 0;
+        public static final int EncPort = 0;
+ 
+        public static final boolean DrivemotorReversed = false;
+        public static final boolean TurnmotorReversed = false;
+ 
+        public static final double kP = 0;
+        public static final double kI = 0;
+        public static final double kD = 0;
+    }
+
+    
+    
+
+    
+}
